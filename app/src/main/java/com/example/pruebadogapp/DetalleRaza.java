da053f0ce33d418e7097a2681077ae4636eebb28
BetoSkater <ajwolfcrow@gmail.com>
2022-05-24 20:35:11 +0200

    Obtención de las imagenes por raza.

--- app/src/main/java/com/example/pruebadogapp/DetalleRaza.java
+++ app/src/main/java/com/example/pruebadogapp/DetalleRaza.java
@@ -5,6 +5,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.apache.http.HttpEntity;
@@ -14,6 +15,7 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -21,6 +23,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class DetalleRaza extends AppCompatActivity {
@@ -37,17 +40,17 @@ public class DetalleRaza extends AppCompatActivity {
 
         //Una vez se tiene la raza, se realiza la consulta al webApi:
 
-        descargarRazaPerros(razaSeleccionada);
+        descargarImagenesRaza(razaSeleccionada);
 
 
 
 
     }
 
-    public void descargarRazaPerros(String criterioBusqueda){
+    public void descargarImagenesRaza(String criterioBusqueda){
         try{
             //Se solicitan cinco imagenes distintas aleatorias de una raza en concreto.
-            String url = "https://dog.ceo/api/"+criterioBusqueda+"breed/hound/images/random/5";
+            String url = "https://dog.ceo/api/breed/"+criterioBusqueda+"/images/random/5";
 
             new APIAsyncTask().execute(url);
 
@@ -103,30 +106,38 @@ public class DetalleRaza extends AppCompatActivity {
         protected void onPostExecute(String informacionObtenida){
             Toast.makeText(getBaseContext(),"Información obtenida con éxito.", Toast.LENGTH_LONG).show();
             try{
-                //Conversión a JSON
-                JSONArray jsonArray = new JSONArray(informacionObtenida);
-                List<String> imagenesURL  = convertirJsonImagenesURL(jsonArray);
+                //La respuesta tiene "message" (contiene todas las urls de las imagenes) y "status". Se almacena en un JSONObject para convertirlo posteriormente a una lista de urls.
+                JSONObject jsonObject = new JSONObject(informacionObtenida);
+
+
+
+                //Paso de JSONObject a lista ArrayList
+
+                List<String> listado = convertirJsonImagenesURL(jsonObject);
 
                 //TODO pasar la lista al Carusel
 
                //Aquí imagino que es donde  tengo que colocar el adaptador del carrusel.
 
             }catch(Exception e){
-
+                Log.e("AsyncTask","Exception",e);
             }
         }
     }
-    public List<String> convertirJsonImagenesURL(JSONArray jsonArray) throws JSONException {
+    public List<String> convertirJsonImagenesURL(JSONObject jsonObject) throws JSONException {
 
-        String resultado = "";
+        //En este caso el mensaje del jsonObject viene como JSONArray, por lo que es mas sencillo de manejar.
 
-        for (int i = 0; i< jsonArray.length(); i++){
-            resultado = jsonArray.getJSONObject(i).optString("message").toString(); // message es lo que aparece en el WebApi, comprobar por si acaso.
+        JSONArray jsonArray = jsonObject.getJSONArray("message");
+
+        List<String> listadoImagenesRaza = new ArrayList<>();
+
+        for(int i = 0; i<jsonArray.length(); i++){
+            listadoImagenesRaza.add(jsonArray.get(i).toString());
         }
 
-        //Según el webApi, el campo mensaje va a contener un string con las url separadas por comas, en este caso cinco urls.
-        List<String> listaImagenes = Arrays.asList(resultado.split(","));
-        return listaImagenes;
+        return listadoImagenesRaza;
+
     }
 
 
